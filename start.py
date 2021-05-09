15e80b7caecf9240e6290cc4ea039c80c8a92498
sonyaku <ms.sonya.0@mail.ru>
2021-05-09 23:10:53 +0300

    start pipeline

--- start.py
+++ start.py
@@ -1,15 +1,17 @@
-import data
+#import data
+import json
 %run libs/libs.py
 %run libs/data.py
+#import os
+#os.system('python libs.py')
 
-###### Глобальные переменные. Они будут переопределены при рестарте пайплайна
-clusters = []
-cluster_with_features = []
-cluster_models = {}
+###### Глобальные переменные. 
 
 config = "C:/Users/msson/energy_artefacts/conf/gisee_test.json.example"
 engine = db.db_connect(config)
 
+saving_path = '/.pipeline/'
+	
 def start_pipline(target = 'ec_d'):
 
 	timeseries = data.get_data(engine)
@@ -21,50 +23,68 @@ def start_pipline(target = 'ec_d'):
 	# удаляем аномалии
 	timeseries_new = []
 	for df in timeseries:
-		timeseries_new.append(delete_anomalies_all(df, engine))
+		timeseries_new.append(delete_anomalies_all(df, plt = False))
 	timeseries = timeseries_new
-	# кластеризуем
+	# кластеризуем и сохраним kmeans
 	clusters = clusterization(timeseries)
+	# выберем репрезентативные ряды из кластеров
 	representatives = []
 	for i in range(clusters.max()):
-		representatives.append(get_claster(timeseries, i, clusters)[0])
+		representatives.append(get_cluster_representative(timeseries, i, clusters)[0])
+	
+	models = {}
 	
 	# выбираем признаки
 	representatives_new = []
-	for representative in representatives:
+	for i in range(len(representatives)):
+		representative = representatives[i]
 		selected = select_features(representative, engine)
 		representatives_new.append(representative[np.append(selected, target)])
+		models[i] = {'features': selected.tolist()}
 	representatives = representatives_new	
+	
 	# выбираем модель
-	models = {}
 	for i in range(clusters.max()):
-		models[i] = select_model(representatives[i], plt = True)
+		models[i]['model'] = select_model(representatives[i], plt = False)
 	
-	
-	'''
-	for df in timeseries:
-		df = delete_anomalies(df, anomalies, type = 'average', num = 7)
+	# сохраняем подобранные модель и признаки у кластеров
+	with open(saving_path + 'cluster_models_info.json', 'w') as fp:
+		json.dump(models, fp)
 		
-	clusters = clustering(timeseries)
+def get_prediction(dt_start_forecast, dt_finish_forecast):
+	# получаем временной ряд для прогноза
+	df = get_data_for_prediction(engine)
+	# получаем доп данные
+	df = get_features(df, engine)
+	# определяем кластер, признаки и модель
+	new_object, columns = get_features_for_clusterization(df)
+	new_object = np.array(new_object).reshape(1,-1)
+	new_object = pd.DataFrame(data = new_object, columns = columns)
+	clst, features, model = find_cluster(new_object, plt = False)
+	# оставляем только нужные признаки
+	df = df[np.append(features, target)]
+	# удаляем аномалии
+	df = delete_anomalies_all(df)
 	
-	clusters_with_features = []
-	for cluster in clusters:
-		clusters_with_features.append(create_features(cluster))
 	
-	for cluster in clusters_with_features:
-		get_features(cluster, engine, lags = False, target = "ec_d")
-	'''	
-	
-	
-def get_prediction(df, period):
-	df = delete_anomalies(df, anomalies, type = 'average', num = 7)
-	cluster, features = get_cluster(clusters)
-	df = get_features(df, cluster)
-	model = cluster_models[cluster]
-	model.fit()
-	prediction = model.predict(period)
-	
+	# получаем exog
+	dates = pd.date_range(dt_start_forecast, dt_finish_forecast, freq='D')
+	exog = pd.DataFrame(index = dates)
+	exog = get_features(exog, engine, lags = False, target = target)
+	exog = exog[features]
+	X_y_train_features =  df
+	y_train = df[target]
+	X_train = df.drop(target, axis = 1)
+	# подбираем гиперпараметры, обучаем модель и делаем предсказание
+	if (model == 'xgb'): 
+		prediction = XGB_forecast(X_train, exog, y_train, y_train)
+	if (model == 'prophet'): 
+		prediction = prophet_forecast(X_y_train_features, exog, features)
+	if (model == 'sarimax'):
+		predicrion = SARIMAX_forecast(X_train, exog, y_train, y_train)
+		
 	return prediction
 
 	
-	
+start_pipline(target = 'ec_d')
+get_prediction( num_pred = 365)
